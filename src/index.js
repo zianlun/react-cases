62c5b3d8b3c10d47691f102b554526ab76ec57ae
ljzhang <3131939824@qq.com>
2023-01-18 00:00:06 +0800

    井字棋案例第一次提交

---
+++ src/index.js
@@ -0,0 +1,169 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+//函数式组件
+function Square(props){
+  return (
+    <button className='square' onClick={() => props.onClick()}> 
+    {props.value}
+    </button>
+  );
+}
+
+/*
+class Square extends React.Component {
+  render() {
+    return (
+      <button 
+      className="square" 
+      onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
+      </button>
+    );
+  }
+}
+*/
+class Board extends React.Component {
+  /*
+  handleClick(i){
+    //深拷贝 ---- 创建一个副本
+    const squares = this.state.squares.slice();
+    if(calculateWinner(squares) || squares[i]){
+      return ;
+    }
+    squares[i] = this.state.flag ? 'X':'O';
+    this.setState({
+      squares:squares,
+      flag:!this.state.flag,
+    });
+  }*/
+  renderSquare(i) {
+    return (
+      <Square 
+        value={this.props.squares[i]} 
+        onClick={() => this.props.onClick(i)}
+      />
+    );
+  }
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      history:[
+        {squares :Array(9).fill(null)}
+      ],
+      stepNumber:0,
+      flag:true,
+    }
+  }
+  handleClick(i){
+    const history = this.state.history.slice(0,this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();  
+    if(calculateWinner(squares) || squares[i]){
+      return ;
+    }
+    squares[i] = this.state.flag ? 'X':'O';
+    this.setState({
+      history:history.concat([
+        {squares}
+      ]),
+      stepNumber:history.length,
+      flag:!this.state.flag,
+    });
+  }
+  jumapTo(step){
+    this.setState({
+      stepNumber:step,
+      flag:(!step%2) ? true:false,
+    })
+  }
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    // console.log(current)
+    let status;
+    const winner = calculateWinner(current.squares);
+    if(winner)
+      status = 'winner:   ' +  winner;
+    else
+      status = 'Next player: ' + (this.state.flag ? 'X' :'o');
+    //操作历史
+    const moves = history.map((step, move) => {
+      const desc = move ?
+      "Go to move" + move :
+      "Go to game start!"
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumapTo(move)}>
+            {desc}
+          </button>
+        </li>
+      )
+    });
+    return (
+      <div className="game">
+        <div 
+          className="game-board"
+        >
+          <Board 
+          squares={current.squares}
+          onClick={(i) => this.handleClick(i)} 
+        />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
